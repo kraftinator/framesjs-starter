8b2649596e3df11134dea9e365a851e1f44a6bb1
Adam Kraft <adamkraft1@gmail.com>
2024-03-28 11:04:58 -0500

    Initial commit

---
+++ app/examples/new-api-cache-control/frames/route.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-key */
+import { Button } from "frames.js/next";
+import { frames } from "./frames";
+
+const handleRequest = frames(async (ctx) => {
+  return {
+    image: (
+      <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex">
+        The current time is {new Date().toLocaleString()}
+      </div>
+    ),
+    imageOptions: {
+      aspectRatio: "1:1",
+    },
+    buttons: [<Button action="post">Refresh</Button>],
+    headers: {
+      // Max cache age in seconds
+      "Cache-Control": "max-age=5",
+    },
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
